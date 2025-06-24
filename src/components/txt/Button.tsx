9d1a1e5a6ebc0e2a6b0f262743ffe48e1328ebe8
MohamCoder <mohammedmus1000@gmail.com>
2025-06-24 11:52:24 +0300

    ui:type fix

--- src/components/txt/Button.tsx
+++ src/components/txt/Button.tsx
@@ -1,7 +1,7 @@
-export function Button({text}: {text: string}) {
+export function Button({text, onClick}: {text: string , onClick?: () => void}) {
   return (
       <div className="h-12 w-24 bg-black rounded-xl flex items-center justify-center">
-        <button className="text-white text-sm font-black select-none">{text}</button>
+        <button className="text-white text-sm font-black select-none" onClick={onClick}>{text}</button>
       </div>
   )
 }
