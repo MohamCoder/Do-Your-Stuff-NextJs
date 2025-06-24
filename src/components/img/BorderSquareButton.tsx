b3a1f7b9c86dc92a9037f438bc90180bf191218b
MohamCoder <mohammedmus1000@gmail.com>
2025-06-24 11:38:31 +0300

    ui:conflict fix

--- src/components/img/BorderSquareButton.tsx
+++ src/components/img/BorderSquareButton.tsx
@@ -7,7 +7,7 @@ export function BorderSquareButton(props : imageButtonProps) {
       className="h-12 w-12 border-2 border-black bg-white rounded-xl flex items-center justify-center"
       onClick={props.onClick}
     >
-      <Image src={props.src} alt={props.alt} width={24} height={24} className="fill-black" />
+      <Image src={props.src} alt={props.alt} width={24} height={24} />
     </div>
   );
 }
--- src/components/img/SquareButton.tsx
+++ src/components/img/SquareButton.tsx
@@ -4,10 +4,10 @@ import { imageButtonProps } from "./types";
 export function SquareButton(props : imageButtonProps) {
   return (
     <div
-      className="h-12 w-12 border-2 border-black bg-white rounded-xl flex items-center justify-center"
+      className="h-12 w-12 bg-black rounded-xl flex items-center justify-center"
       onClick={props.onClick}
     >
-      <Image src={props.src} alt={props.alt} width={24} height={24}  />
+      <Image src={props.src} alt={props.alt} width={24} height={24} className="invert"/>
     </div>
   );
 }
