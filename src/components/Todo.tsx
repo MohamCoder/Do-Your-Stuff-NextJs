095a09d50e16ec900fa8b7d84613d73e2adb38a8
MohamCoder <mohammedmus1000@gmail.com>
2025-06-24 10:55:39 +0300

    ui:scale and color changes

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,16 +3,19 @@ import { Button } from "./txt/Button";
 
 export function Todo() {
   return (
-    <div className="mb-4 flex justify-between w-full">
-      <span>
-        <h2 className="text-2xl" id="title">
-          workout
-        </h2>
-        <h6 className="text-xs">for 3 hours</h6>
-      </span>
-      <div className="flex gap-4">
-        <BorderSquareButton url="X" />
-        <Button text="done" />
+    <div>
+      <div className="h-2 w-2 rotate-45 bg-white border-black inline-block border-2 translate-y-[29px]"></div>
+      <div className="mb-4 ml-4 flex justify-between w-full">
+        <span>
+          <h2 className="font-medium text-2xl" id="title">
+            workout
+          </h2>
+          <h6 className="text-xs opacity-75">for 3 hours</h6>
+        </span>
+        <div className="flex gap-4">
+          <BorderSquareButton url="X" />
+          <Button text="done" />
+        </div>
       </div>
     </div>
   );
